e4fc633c23c2b1517fb153742f27f4d3c515eb85
JordenPowleyWebDev <jorden.powley.webdev@gmail.com>
2022-01-04 22:24:33 +0000

    Add Initial Button Component.

--- config/laravel-components.php
+++ config/laravel-components.php
@@ -2,4 +2,9 @@
 
 return [
     'views-namespace' => 'laravel-components',
+    'default-classes'    => [
+        'components'    => [
+            'button'    => "px-4 font-weight-bold"
+        ],
+    ],
 ];
