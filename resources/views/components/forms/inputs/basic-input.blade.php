2bf64861c6d1dcf54b5789369916e897914a8d7a
JordenPowleyWebDev <jorden.powley.webdev@gmail.com>
2022-01-24 20:56:59 +0000

    Re-Add Attributes To Input.

--- resources/views/components/forms/inputs/basic-input.blade.php
+++ resources/views/components/forms/inputs/basic-input.blade.php
@@ -5,9 +5,9 @@
     class="{{ $classes['container'] }} @error($name){{ config('laravel-components.default-classes.components.form.inputs.input.invalid') }} @enderror"
     @if(isset($required) && $required === true)required @endif
     @if(isset($value) && filled($value))value="{{ $value }}" @endif
-{{--    @if(isset($inputAttributes) && (sizeOf($inputAttributes) > 0))--}}
-{{--        @foreach($inputAttributes as $key => $attribute)--}}
-{{--            {{ $key }}="{{ $attribute }}"--}}
-{{--        @endforeach--}}
-{{--    @endif--}}
+    @if(isset($inputAttributes) && (sizeOf($inputAttributes) > 0))
+        @foreach($inputAttributes as $key => $attribute)
+            {{ $key }}="{{ $attribute }}"
+        @endforeach
+    @endif
 />
