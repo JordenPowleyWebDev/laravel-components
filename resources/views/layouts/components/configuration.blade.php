ea9f5dbf8ee98674f0fd8a812c520200c5890781
JordenPowleyWebDev <jorden.powley.webdev@gmail.com>
2022-01-27 22:14:34 +0000

    Fix Configuration.

--- resources/views/layouts/components/configuration.blade.php
+++ resources/views/layouts/components/configuration.blade.php
@@ -1,7 +1,7 @@
 <script>
     window.laravelComponents = {
-        'views-namespace': "{{ json_encode(config('laravel-components.views-namespace')) }}",
-        'empty-value': "{{ json_encode(config('laravel-components.empty-value')) }}",
-        'default-classes': "{{ json_encode(config('laravel-components.default-classes')) }}",
+        'views-namespace': @json(config('laravel-components.views-namespace')),
+        'empty-value': @json(config('laravel-components.empty-value')),
+        'default-classes': @json(config('laravel-components.default-classes')),
     }
 </script>
