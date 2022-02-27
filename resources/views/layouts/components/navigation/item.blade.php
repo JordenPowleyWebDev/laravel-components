78a25a6dcdd5b9197a0806bc763b56e68a58079b
JordenPowleyWebDev <jorden.powley.webdev@gmail.com>
2022-02-27 18:47:08 +0000

    Fix Permission Checks On Navigation.

--- resources/views/layouts/components/navigation/item.blade.php
+++ resources/views/layouts/components/navigation/item.blade.php
@@ -17,11 +17,13 @@ if (isset($active) && $active === true) {
 }
 
 @endphp
-<a href="{{ isset($link) && filled($link) ? $link : route($name) }}" class="{{ $classes }}">
-    @if(isset($icon) && filled($icon))
-        <i class="{{ $icon }} {{ config('laravel-components.views-namespace')."-navigation-item-icon ".config('laravel-components.default-classes.layouts.navigation.item.icon') }}"></i>
-    @elseif(isset($grouped) && $grouped)
-        <i class="{{ config('laravel-components.views-namespace')."-navigation-item-grouped-icon ".config('laravel-components.default-classes.layouts.navigation.item.grouped-icon') }}" style="color: transparent;"></i>
-    @endif
-    <span class="{{ config('laravel-components.views-namespace')."-navigation-item-label ".config('laravel-components.default-classes.layouts.navigation.item.label') }}">{{ $label }}</span>
-</a>
+@if (!isset($can) || (isset($can) && array_key_exists('permission', $can) && array_key_exists('model', $can) && Auth::user()->can($can['permission'], $can['model'])))
+    <a href="{{ isset($link) && filled($link) ? $link : route($name) }}" class="{{ $classes }}">
+        @if(isset($icon) && filled($icon))
+            <i class="{{ $icon }} {{ config('laravel-components.views-namespace')."-navigation-item-icon ".config('laravel-components.default-classes.layouts.navigation.item.icon') }}"></i>
+        @elseif(isset($grouped) && $grouped)
+            <i class="{{ config('laravel-components.views-namespace')."-navigation-item-grouped-icon ".config('laravel-components.default-classes.layouts.navigation.item.grouped-icon') }}" style="color: transparent;"></i>
+        @endif
+        <span class="{{ config('laravel-components.views-namespace')."-navigation-item-label ".config('laravel-components.default-classes.layouts.navigation.item.label') }}">{{ $label }}</span>
+    </a>
+@endif
