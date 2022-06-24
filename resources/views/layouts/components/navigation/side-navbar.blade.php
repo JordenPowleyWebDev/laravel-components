904cc7b569ab88a4e674ec61482d8e6cb14f8c72
JordenPowleyWebDev <jorden.powley.webdev@gmail.com>
2022-06-24 23:21:20 +0100

    Fix Routing.

--- resources/views/layouts/components/navigation/side-navbar.blade.php
+++ resources/views/layouts/components/navigation/side-navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="{{ config('laravel-components.views-namespace')."-navigation-side-navbar-container ".config('laravel-components.default-classes.layouts.navigation.side-navbar.container') }}" id="side-navbar">
     <div class="{{ config('laravel-components.views-namespace')."-navigation-side-navbar-nav-logo ".config('laravel-components.default-classes.layouts.navigation.side-navbar.nav-logo') }}">
-        <a href="{{ route('login') }}" class="{{ config('laravel-components.views-namespace')."-navigation-side-navbar-nav-link ".config('laravel-components.default-classes.layouts.navigation.side-navbar.nav-link') }}">{{ config('app.name') }}</a>
+        <a href="{{ route(config('laravel-components.nav-home-name')) }}" class="{{ config('laravel-components.views-namespace')."-navigation-side-navbar-nav-link ".config('laravel-components.default-classes.layouts.navigation.side-navbar.nav-link') }}">{{ config('app.name') }}</a>
     </div>
     @include(config('laravel-components.views-namespace').'::layouts.components.navigation.array')
 </nav>
