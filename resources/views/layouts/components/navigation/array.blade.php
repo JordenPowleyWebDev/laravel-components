6793c0e36231174e0499ec8b4be9db8e85438394
Jorden Powley <31571981+JordenPowleyWebDev@users.noreply.github.com>
2023-03-20 16:00:25 +0000

    Add Roles To Navigation Array.

--- resources/views/layouts/components/navigation/array.blade.php
+++ resources/views/layouts/components/navigation/array.blade.php
@@ -1,9 +1,11 @@
 @foreach (config('navigation.items') as $navItem)
-    @if (!array_key_exists('can', $navItem) || (array_key_exists('can', $navItem) && array_key_exists('permission', $navItem['can']) && array_key_exists('model', $navItem['can']) && Auth::user()->can($navItem['can']['permission'], $navItem['can']['model'])))
-        @if (array_key_exists('type', $navItem) && $navItem['type'] === "grouped")
-            @include(config('laravel-components.views-namespace').'::layouts.components.navigation.group', $navItem)
-        @else
-            @include(config('laravel-components.views-namespace').'::layouts.components.navigation.item', $navItem)
+    @if (!array_key_exists('roles', $navItem) || (array_key_exists('roles', $navItem) && Auth::user()->hasAnyRole($navItem['roles'], $navItem['roles'])))
+        @if (!array_key_exists('can', $navItem) || (array_key_exists('can', $navItem) && array_key_exists('permission', $navItem['can']) && array_key_exists('model', $navItem['can']) && Auth::user()->can($navItem['can']['permission'], $navItem['can']['model'])))
+            @if (array_key_exists('type', $navItem) && $navItem['type'] === "grouped")
+                @include(config('laravel-components.views-namespace').'::layouts.components.navigation.group', $navItem)
+            @else
+                @include(config('laravel-components.views-namespace').'::layouts.components.navigation.item', $navItem)
+            @endif
         @endif
     @endif
 @endforeach
